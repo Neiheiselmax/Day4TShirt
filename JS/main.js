ecea1c55dd1e9ff80d13ffa1f2d8dbdd8e10480a
Max Neiheisel <maxneiheisel@Max-Neiheisels-MacBook-2.local>
2017-03-14 09:45:13 -0400

    Initial

--- JS/main.js
+++ JS/main.js
@@ -1,59 +1,64 @@
 var bluebutton = document.getElementById('bluecart');
 var pinkbutton = document.getElementById('pinkcart');
 var orangebutton = document.getElementById('orangecart');
-var subtotal = document.getElementById('subtotal');
-var total = document.getElementById('total');
+var subtotalBox = document.getElementById('subtotal');
+var totalBox = document.getElementById('total');
 var bluePressed = false;
 var pinkPressed = false;
 var orangePressed = false;
 var beforetax = 0;
+var kart = document.getElementById('cart');
 
-
-function calculateTotal(subtotal) {
-  return (subtotal*1.0625).toFixed(2);
+var blueShirt = {
+  name:"Save My Trees",
+  price: 29,
+}
+var pinkShirt = {
+  name:"Nature Lover",
+  price: 19,
+}
+var orangeShirt = {
+  name:"Forrest Walk",
+  price: 39,
 }
 
+var whatsInMyCart = [];
 
+
+
+function updateCart(){
+  var total = 0;
+  whatsInMyCart.forEach(function(shirt){
+     total+=shirt.price;
+})
+
+  subtotalBox.innerHTML = subtotal;
+
+
+  var total = subtotal*1.065;
+
+  totalBox.inneHTML = total.toFixed(2);
+  
+  }
+    
 
 
 bluebutton.addEventListener('click',function(){
-  if(bluePressed===false){
-  	bluePressed = true;
-  	beforetax +=29;
-  }
-  else{
-  	bluePressed = false;
-  	beforetax -=29;
-  }
-  subtotal.innerHTML = beforetax;
-  total.innerHTML = calculateTotal(beforetax)
+  whatsInMyCart.push(blueShirt);
+  updateCart();
 })
 
 pinkbutton.addEventListener('click',function(){
-  if(pinkPressed===false){
-  	pinkPressed = true;
-  	beforetax +=19;
-  }
-  else{
-  	pinkPressed = false;
-  	beforetax -=19;
-  }
-  subtotal.innerHTML = beforetax;
-  total.innerHTML = calculateTotal(beforetax)
+    whatsInMyCart.push(pinkShirt);
+    updateCart();
 })
 
 orangebutton.addEventListener('click',function(){
-  if(orangePressed===false){
-  	orangePressed = true;
-  	beforetax +=39;
-  }
-  else{
-  	orangePressed = false;
-  	beforetax -=39;
-  }
-  subtotal.innerHTML = beforetax;
-  total.innerHTML = calculateTotal(beforetax)
+    whatsInMyCart.push(orangeShirt);
+    updateCart();
 })
 
+
+//Add to cart//
 
 
